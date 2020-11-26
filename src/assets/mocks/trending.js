59c683d2b2cdd83235945d517f85e22dddd49bdc
James Meyer <Jamesmeyer75@gmail>
2020-11-26 16:39:53 -0600

    Added home page layout with test data for display, still have some mobile work to do, but it is mostly responsive

---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-anonymous-default-export */
+import moment from 'moment';
+
+export default [
+    {
+        title: 'Sofeware Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg'
+    },
+    {
+        title: 'GraphQL vs REST',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'JavaScript'],
+        link: '#',
+        image: 'graphQL-vs-rest.png'
+    },
+    {
+        title: 'A Day in the Life of a Programmer',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'day-life.png'
+    },
+    {
+        title: 'Brain Hacks for Learning to Program',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'hacks-for-brain.jpeg'
+    },
+    {
+    title: 'Popular Programming Languages in 2020',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech News', 'Tech Culture'],
+    link: '#',
+    image: 'Top-languages.jpeg'
+    }
+]
